d0b03ba0726e43ba7f0be3e610ce04552d3a41d6
gany-15 <ganesharkanath15@gmail.com>
2019-11-13 16:32:48 +0530

    prog6

--- WT/prog6.php
+++ WT/prog6.php
@@ -1,1 +1,11 @@
 <?php
+    print "<h3>REFRESH PAGE TO INCREMENT COUNTER</h3>";
+    $name = "prog6-Counter.txt";
+    $file = fopen($name, "r");
+    $hits = fscanf($file, "%d");
+    fclose($file);
+    $hits[0]++;
+    $file = fopen($name, "w");
+    fprintf($file, "%d", $hits[0]);
+    fclose($file);
+    print "Total Views: ". $hits[0];
